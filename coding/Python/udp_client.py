f5f2671afb62a1dea645cdf0ba784339a38a3297
stilla1ex <alexmwangimungai254@gmail.com>
2025-06-16 16:14:15 +0300

    Restore deleted files and updating the main file

--- coding/Python/udp_client.py
+++ coding/Python/udp_client.py
@@ -9,7 +9,7 @@ target_port = 80
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # send some data
-client.sendto('AAABBBCCC'.encode(), (target_host, target_port))
+client.sendto(b"AAABBBCCC", (target_host, target_port))
 
 data, addr = client.recvfrom(4096)
 
