896ed09a869c5cfad7f5afa42d380692da2381a5
stilla1ex <alexmwangimungai254@gmail.com>
2025-06-16 15:13:36 +0300

    Restore deleted files

--- coding/Python/tcp_client.py
+++ coding/Python/tcp_client.py
@@ -5,15 +5,15 @@ import socket
 target_host = 'google.com'
 target_port = 80
 
-# create the socket client
+#create the socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect
+# connect the client to the target host and port
 client.connect((target_host, target_port))
 
 # send some data
 client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n".encode())
 
-response = client.recv(4000)
+responce = client.recv(4096)
 
-print(response) # print the response
+print(responce)
